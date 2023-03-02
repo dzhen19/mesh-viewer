c08a694e60c53abbc3ce7e0707300fe825fd5e7f
Derrick Zhen <derrickzhen@Derricks-MacBook-Air.local>
2023-03-01 23:47:17 -0500

    added mouse + wheel controls

--- src/mesh-viewer.cpp
+++ src/mesh-viewer.cpp
@@ -16,6 +16,15 @@ using namespace std;
 using namespace glm;
 using namespace agl;
 
+glm::vec3 crossProduct(vec3 a, vec3 b)
+{
+   // (a2 * b3 – a3 * b2) * i + (a3 * b1 – a1 * b3) * j + (a1 * b2 – a2 * b1) * k,
+   return vec3(
+       a[1] * b[2] - a[2] * b[1],
+       a[2] * b[0] - a[0] * b[2],
+       a[0] * b[1] - a[1] * b[0]);
+}
+
 string getPathFromIndex(int idx)
 {
    string filename = (GetFilenamesInDir("../models", "ply"))[idx];
@@ -28,35 +37,101 @@ class MeshViewer : public Window
 {
 public:
    int meshIdx = 0;
+   float scaleFactor = 1;
+   float translateX = 0;
+   float translateY = 0;
+   float translateZ = 0;
+
+   float radius = 10;
+   // in degrees [0, 360]
+   float azimuth = 180;
+   // in degrees [-90, 90]
+   float elevation = 90;
+
+   bool canRotate = false;
 
    MeshViewer() : Window()
    {
       setup();
    }
 
-   void setup()
+   vec3 getEyePos()
    {
-      mesh.load(getPathFromIndex(meshIdx));
+      return vec3(radius * sin(radians(azimuth)) * cos(radians(elevation)),
+                  radius * sin(radians(elevation)),
+                  radius * cos(radians(azimuth)) * cos(radians(elevation)));
    }
 
+   vec3 getUpPos()
+   {
+      // z directional vector is just the vector going from 0,0,0 to eyePos, so eyePos
+      vec3 zVec = getEyePos();
+      // x directional vector is z vec crossed with arbitrary unit y
+      vec3 xVec = crossProduct(zVec, vec3(0, 1, 0));
+      vec3 yVec = crossProduct(zVec, xVec);
+      return yVec;
+   }
+
+   void setup()
+   {
+      PLYMesh newMesh;
+      newMesh.load(getPathFromIndex(meshIdx));
+      mesh = newMesh;
+
+      // calculate translate based on model origin
+      vec3 max = mesh.maxBounds();
+      vec3 min = mesh.minBounds();
+      float dx = max[0] - min[0];
+      float dy = max[1] - min[1];
+      float dz = max[2] - min[2];
+
+      translateX = -(max[0] - (dx / 2));
+      translateY = -(max[1] - (dy / 2));
+      translateZ = -(max[2] - (dz / 2));
+
+      // calculate scale factor based on max bounds
+      float maxD = std::max(dx, (std::max(dy, dz)));
+      scaleFactor = 10 / maxD;
+
+      // reset eye position
+      radius = 10;
+      azimuth = 180;
+      elevation = 90;
+   }
+
+   // need to fix the math on this
    void mouseMotion(int x, int y, int dx, int dy)
    {
-      // std::cout << x << " " << y << " " << dx << " " << dy << std::endl;
-      // std::cout << key << std::endl;
-      // std::cout << key << std::endl;
-      // std::cout << key << std::endl;
+      if (!canRotate)
+      {
+         return;
+      }
+
+      std::cout << "elevation: " << elevation << " azimuth: " << azimuth << " radius: " << radius << std::endl;
+
+      float newAzimuth = azimuth + dx;
+      azimuth = std::max(std::min(newAzimuth, float(360)), float(0.1));
+
+      float newElevation = elevation + dy;
+      elevation = std::max(std::min(newElevation, float(90)), float(-90));
    }
 
    void mouseDown(int button, int mods)
    {
+
+      canRotate = true;
    }
 
    void mouseUp(int button, int mods)
    {
+      canRotate = false;
    }
 
    void scroll(float dx, float dy)
    {
+      float newRadius = radius += dy / 10;
+      // radius should not be negative
+      radius = std::max(newRadius, float(.01));
    }
 
    void keyUp(int key, int mods)
@@ -87,29 +162,30 @@ public:
             meshIdx--;
          }
       }
-      PLYMesh newMesh;
-      newMesh.load(getPathFromIndex(meshIdx));
-      mesh = newMesh;
+
+      setup();
    }
 
    void draw()
    {
+      eyePos = getEyePos();
+      up = getUpPos();
 
       float aspect = ((float)width()) / height();
       renderer.perspective(glm::radians(60.0f), aspect, 0.1f, 50.0f);
       renderer.lookAt(eyePos, lookPos, up);
 
-      renderer.translate(vec3(0, 0, 0));
+      renderer.translate(vec3(translateX, translateY, translateZ));
       renderer.rotate(vec3(0, 0, 0));
-      renderer.scale(vec3(1, 1, 1));
+      renderer.scale(vec3(scaleFactor, scaleFactor, scaleFactor));
       renderer.mesh(mesh);
    }
 
 protected:
    PLYMesh mesh;
-   vec3 eyePos = vec3(10, 0, 0);
+   vec3 eyePos = getEyePos();
    vec3 lookPos = vec3(0, 0, 0);
-   vec3 up = vec3(0, 1, 0);
+   vec3 up = getUpPos();
 };
 
 int main(int argc, char **argv)
